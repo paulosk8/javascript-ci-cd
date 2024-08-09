0856d2787b6b5073236cd9f787f756866a8e54fd
Paulo Galarza <paulo-123@hotmail.es>
2024-08-08 19:51:45 -0500

    Cambio en package y test

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
 const sum = require('./index');
-test('Suma los valores 4 + 3 es igual a 7', () => {
-  expect(sum(4, 3)).toBe(7);
+test('Suma los valores 4 + 4 es igual a 8', () => {
+  expect(sum(4, 4)).toBe(8);
 });
